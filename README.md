f098501b8eee36e6669ddc35db1278a69d6aaebb
Jmeas <jellyes2@gmail.com>
2015-02-08 11:28:22 -0500

    Begin work.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # hsm.js
-A hierarchal state machine for Javascript.
+[![Travis build status](http://img.shields.io/travis/6to5/6to5-library-boilerplate.svg?style=flat)](https://travis-ci.org/6to5/6to5-library-boilerplate)
+[![Code Climate](https://codeclimate.com/github/6to5/6to5-library-boilerplate/badges/gpa.svg)](https://codeclimate.com/github/6to5/6to5-library-boilerplate)
+[![Test Coverage](https://codeclimate.com/github/6to5/6to5-library-boilerplate/badges/coverage.svg)](https://codeclimate.com/github/6to5/6to5-library-boilerplate)
+[![Dependency Status](https://david-dm.org/6to5/6to5-library-boilerplate.svg)](https://david-dm.org/6to5/6to5-library-boilerplate)
+[![devDependency Status](https://david-dm.org/6to5/6to5-library-boilerplate/dev-status.svg)](https://david-dm.org/6to5/6to5-library-boilerplate#info=devDependencies)
+
+A minimal hierarchal state machine for Javascript.
+
+### What is a state machine?
+
+A state machine is a thing that consists of a number of states. At any time, it can
+only be in exactly one state at a time.
+
+The name 'state machine' is shorthand for 'finite state machine,' which describes another
+property of these objects: they have a finite number of states.
+
+### What is a hierarchal state machine?
+
+A state machine that has a notion of nested states is called a hierarchal state machine. When
+a nested state is active, all of its parent states are also active.
+
+### What does this library do?
+
+This library provides the scaffolding to represent a system like the above.
+
+### What doesn't this library do?
+
+This library is intentionally unopinionated about two things:
+
+1. What a state is
+2. What happens when you transition between states
+
+The point is that you can use this library to define more opinionated states
+and the mechanism of transitions to solve problems.
+
+### API
+
---
+++ src/hsm.js
@@ -0,0 +1,76 @@
+import _ from 'underscore';
+import State from './state';
+
+var Hsm = function(options) {
+  options = options || {};
+  this._states = {};
+  this._currentState = '';
+  var states = options.states;
+  for (var stateName in states) {
+    if(!states.hasOwnProperty(stateName)){ return; }
+    this.setState(stateName, states[stateName]);
+  }
+};
+
+Hsm.prototype.isValidState = function(stateName) {
+  if (/\./.test(stateName)) {
+    return stateName.length === 1;
+  }
+  return true;
+};
+
+Hsm.prototype.validateState = function(stateName) {
+  if (Hsm.isValidState(stateName)) {
+    throw new Error('State names with a period can contain no other characters.');
+  }
+};
+
+Hsm.prototype.setState = function(stateName, stateDef) {
+  this.validateState(stateName);
+  this._states[stateName] = new this.State(stateName, stateDef, this);
+};
+
+Hsm.prototype.currentState = function() {
+  return this._currentState;
+};
+
+Hsm.prototype.getState = function(stateName) {
+  return this._states[stateName];
+};
+
+Hsm.prototype.transitionTo = function(newState) {
+  if (this.currentState === newState) { return; }
+  if (!this.hasState(newState)) { return; }
+  var stop = false;
+  var cancel = function() { stop = true; };
+  var diff = this.diff(this._currentState, newState);
+  Promise.resolve(this.willTransition(diff, cancel))
+    .then(function() {
+      if (stop) { return false; }
+      this._currentState = newState;
+    });
+};
+
+Hsm.prototype.diff = function(start, end) {
+  var startStates = start.split('.');
+  var endStates = end.split('.');
+  for (var i = 0; i < startStates.length; i++) {
+    if (startStates[i] !== endStates[i]) {  break; }
+  }
+
+  return {
+    exitStates: _.rest(startStates, i).reverse(),
+    enterStates: _.rest(endStates, i)
+  };
+};
+
+Hsm.prototype.hasState = function(stateName) {
+  return this._states.indexOf(stateName) >= 0;
+};
+
+// Write your own transition
+Hsm.prototype.willTransition = function(diff, cancel) {};
+
+Hsm.prototype.State = State;
+
+export default Hsm;
